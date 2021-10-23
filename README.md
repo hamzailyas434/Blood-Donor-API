b227ca6ea183ee9f911b7e4c52979cb0ebb75596
Hamza Ilyas <hamzailyas434@gmail.com>
2021-10-24 02:35:38 +0500

    Password & Confirm Password Validation, and Encryption the Password

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 
 
 ## Create Signup User
+
+Add Password and Confirm Password Custom Validation
+
+## Add Password Encryption Hash 
+### install  bcryptjs
+npm i bcryptjs
+
+https://www.npmjs.com/package/bcryptjs
+
+Add this into your model
+const bcrypt = require('bcryptjs');
+
+// Encryption add Between getting the data and adding in the database
+userSchema.pre('save', async function (next) {
+    // Only run this function if password was actually modified
+    if (!this.isModified('password')) return next();
+
+    //  Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    // Delete the confirm password at this time we have the password in hash form
+    this.passwordConfirm = undefined;
+    //  call next()
+    next();
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 // Import Validator for email validation
 const validator = require('validator');
+//Add Hash Encryption in Password
+const bcrypt = require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         trim: true,
-        required: [true, 'Please tell us your name']
+        required: [true, 'Please enter your name']
     },
     email: {
         type: String,
         trim: true,
-        required: [true, 'Please provide your email'],
+        required: [true, 'Please enter your email'],
         unique: true,
         lowercase: true,
         validate: [validator.isEmail, 'Please provide a valid email']
@@ -23,10 +26,28 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your Password']
+        required: [true, 'Please confirm your Password'],
+        validate: {
+            // This is only works on Create and SAVE !!
+            validator: function (el) {
+                return el === this.password;  // abc === abc
+            },
+            message: 'Passwords are not the same'
+        }
     }
 
 });
+// Encryption add Between getting the data and adding in the database
+userSchema.pre('save', async function (next) {
+    // Only run this function if password was actually modified
+    if (!this.isModified('password')) return next();
 
+    //  Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    // Delete the confirm password at this time we have the password in hash form
+    this.passwordConfirm = undefined;
+    //  call next()
+    next();
+});
 const User = mongoose.model('User', userSchema);
 module.exports = User;

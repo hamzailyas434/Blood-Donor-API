fca22bad98287b3d1e10455af76d78abd50ba089
Hamza Ilyas <hamzailyas434@gmail.com>
2021-10-24 02:07:57 +0500

    catching error in seperate async function

--- controllers/donorController.js
+++ controllers/donorController.js
@@ -1,87 +1,63 @@
 const Donor = require('./../models/donorModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllDonors = async (req, res) => {
-    try {
-        const donors = await Donor.find();
-        res.status(200).json({
-            status: "success",
-            results: donors.length,
-            data: {
-                donors,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            message: err,
-        });
-    }
-};
-exports.getDonor = async (req, res) => {
-    try {
-        const donor = await Donor.findById(req.params.id);
-        res.status(200).json({
-            status: "success",
-            data: {
-                donor,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: err,
-        });
-    }
-};
+exports.getAllDonors = catchAsync(async (req, res, next) => {
 
-exports.createDonor = async (req, res) => {
-    try {
-        const createDonor = await Donor.create(req.body);
-        res.status(200).json({
-            status: "success",
-            data: {
-                createDonor,
-            },
-        });
+    const donors = await Donor.find();
+    res.status(200).json({
+        status: "success",
+        results: donors.length,
+        data: {
+            donors,
+        },
+    });
 
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: err,
-        });
-    }
-};
+});
+exports.getDonor = catchAsync(async (req, res, next) => {
 
-exports.deleteDonor = async (req, res) => {
-    try {
-        const deleteDonor = await Donor.findByIdAndDelete(req.params.id);
-        res.status(200).json({
-            status: "success",
-            data: {
-                deleteDonor,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: err,
-        });
-    }
-};
+    const donor = await Donor.findById(req.params.id);
+    res.status(200).json({
+        status: "success",
+        data: {
+            donor,
+        },
+    });
 
-exports.updateDonor = async (req, res) => {
-    try {
-        const updateDonor = await Donor.findByIdAndUpdate(req.params.id, req.body);
-        res.status(200).json({
-            status: "success",
-            data: {
-                updateDonor,
-            },
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: "fail",
-            message: err,
-        });
-    }
-};
+});
+
+exports.createDonor = catchAsync(async (req, res, next) => {
+
+    const createDonor = await Donor.create(req.body);
+    res.status(200).json({
+        status: "success",
+        data: {
+            createDonor,
+        },
+    });
+});
+
+exports.deleteDonor = catchAsync(async (req, res, next) => {
+
+    const deleteDonor = await Donor.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+        status: "success",
+        data: {
+            deleteDonor,
+        },
+    });
+
+});
+
+
+exports.updateDonor = catchAsync(async (req, res, next) => {
+
+    const updateDonor = await Donor.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+        status: "success",
+        data: {
+            updateDonor,
+        },
+    });
+
+});
+

33f569f737a94c12d330733f3c1ed01ac90a5c8c
Hamza Ilyas <hamzailyas434@gmail.com>
2021-10-24 01:46:32 +0500

    Working on Global Error

--- app.js
+++ app.js
@@ -15,11 +15,25 @@ app.use('/api/v1/donors', donorRoute);
 
 // Global Error Handling  Original: 127.0.0.1:3000/api/v1/donors Error: 127.0.0.1:3000/api/v1/donor
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // });
+    // next();
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 400;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     });
-    next();
 });
 
 module.exports = app;
